89b8dcffe10fc12b646f19b3f51bf8fb8a884479
carlostene-svg <carlos.tene@uisek.edu.ec>
2025-11-14 09:21:57 -0500

    Examen Parcial 1 Terminado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    gender = models.CharField(max_length=60, null=False)
+    director_name = models.CharField(max_length=80, null=False)
+    publication_year = models.IntegerField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} {self.publication_year}"
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Movie
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'movies': movies,
+    }
+    return HttpResponse(template.render(context, request))
+
+def movie(request, movie_id):
+    movie = Movie.objects.get(id = movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
